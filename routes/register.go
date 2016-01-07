f8a07f71264c8bb268fa3cb1c131902dc1f2f9ac
Nick Landolfi <nclandolfi@gmail.com>
2016-01-06 19:28:49 -0800

    register form

--- routes/register.go
+++ routes/register.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"log"
+
 	"github.com/elos/ehttp/serve"
 	"github.com/elos/ehttp/templates"
 	"github.com/elos/mist/services"
 	"github.com/elos/mist/views"
+	"github.com/elos/models"
 )
 
 func RegisterGET(c *serve.Conn, engine services.Views) {
@@ -12,5 +15,31 @@ func RegisterGET(c *serve.Conn, engine services.Views) {
 }
 
 func RegisterPOST(c *serve.Conn, db services.DB) {
-	c.Write([]byte("Register Post"))
+	u := models.NewUser()
+	u.SetID(db.NewID())
+	err := db.Save(u)
+	if err != nil {
+		log.Printf("%s", err)
+		c.Write([]byte(err.Error()))
+		return
+	}
+
+	p := models.NewPerson()
+	p.SetID(db.NewID())
+	p.SetOwner(u)
+
+	name := c.ParamVal("name")
+	phone := c.ParamVal("phone")
+
+	p.Name = name
+	p.Phone = phone
+
+	err = db.Save(p)
+	if err != nil {
+		log.Printf("%s", err)
+		c.Write([]byte(err.Error()))
+		return
+	}
+
+	c.Write([]byte("Heck Yeah"))
 }
