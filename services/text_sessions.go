b4a90111ec57ebd7682e3c5d956333b873b0c891
Nick Landolfi <nclandolfi@gmail.com>
2016-01-10 16:42:39 -0800

    swap in real cli backing

--- services/text_sessions.go
+++ services/text_sessions.go
@@ -1,9 +1,13 @@
 package services
 
 import (
+	"log"
+	"strings"
 	"time"
 
 	"github.com/elos/echo"
+	"github.com/elos/elos/command"
+	"github.com/mitchellh/cli"
 )
 
 func NewTexts(twilio Twilio) *TextSessions {
@@ -37,7 +41,11 @@ func (ts *TextSessions) Run(twilio Twilio) chan<- struct{} {
 				if !ok {
 					ts.sessions[e.From] = session(e.From, bails, twilio)
 				}
-				ts.sessions[e.From] <- e.Body
+				// to prevent deadlock between a session that we are
+				// trying to send to bailing
+				go func() {
+					ts.sessions[e.From] <- e.Body
+				}()
 			case s := <-bails:
 				if _, ok := ts.sessions[s]; ok {
 					delete(ts.sessions, s)
@@ -59,16 +67,44 @@ func session(from string, bail chan<- string, twilio Twilio) chan<- string {
 		for {
 			select {
 			case text := <-input:
-				twilio.Send(from, text)
+				// Construct a new CLI with our name and version
+				c := cli.NewCLI("elos [mist]", "0.0.1")
 
-			case <-time.After(5 * time.Minute):
-				if from != "" {
-					twilio.Send(from, "Session time out")
+				// Tokenize the message
+				c.Args = strings.Split(text, " ")
+
+				ui, out := echo.NewTextUI(input, "TODO")
+				go func() {
+					for s := range out {
+						twilio.Send(from, s)
+					}
+				}()
+
+				// Initialize the commands
+				c.Commands = map[string]cli.CommandFactory{
+					"note": func() (cli.Command, error) {
+						return &command.NoteCommand{
+							Ui: ui,
+							Config: &command.Config{
+								DB: "localhost",
+							},
+						}, nil
+					},
 				}
+
+				// now we block on run, so someone else can pop from input,
+				// namely the UI
+				_, err := c.Run()
+				if err != nil {
+					log.Print(err.Error())
+				}
+			case <-time.After(5 * time.Minute):
+				twilio.Send(from, "Session time out")
 				bail <- from
 				break Run
 			}
 		}
+
 	}()
 
 	return input
